4b96ff1159de7fc261b16db5122512d086affa45
Raju Raghuwanshi <rajuraghuwanshi93@gmail.com>
2020-05-10 17:13:04 +0530

    changed variable name

--- week2/Solution3.java
+++ week2/Solution3.java
@@ -8,19 +8,19 @@ public class Solution3 {
     //runtime beats 100% of the java submissions
     public int findJudge(int N, int[][] trust) {
 
-        int []map = new int[N+1];
+        int[] map = new int[N + 1];
 
         int len = trust.length;
 
-        for(int i=0; i<len; i++){
+        for (int i = 0; i < len; i++) {
 
             map[trust[i][1]]++;
         }
 
-        for(int i=1; i<N+1; i++){
-            if(map[i]==N-1){
-                for(int j=0; j<len; j++){
-                    if(trust[j][0]==i){
+        for (int i = 1; i < N + 1; i++) {
+            if (map[i] == N - 1) {
+                for (int j = 0; j < len; j++) {
+                    if (trust[j][0] == i) {
                         return -1;
                     }
                 }
@@ -32,22 +32,20 @@ public class Solution3 {
     }
 
     //runtime beats 86% of the java submissions
-    public int findJudge2(int N, int[][] trust) {
 
-        int []map = new int[N+1];
-        int []map2 = new int[N+1];
+    public int findJudge(int N, int[][] trust) {
 
+        int[] inDegree = new int[N + 1];
+        int[] outDegree = new int[N + 1];
         int len = trust.length;
 
-        for(int i=0; i<len; i++){
-
-            map[trust[i][1]]++;
-            map2[trust[i][0]]++;
-
+        for (int i = 0; i < len; i++) {
+            inDegree[trust[i][1]]++;
+            outDegree[trust[i][0]]++;
         }
 
-        for(int i=1; i<N+1; i++){
-            if(map[i]==N-1 && map2[i]==0){
+        for (int i = 1; i < N + 1; i++) {
+            if (inDegree[i] == N - 1 && outDegree[i] == 0) {
                 return i;
             }
         }
